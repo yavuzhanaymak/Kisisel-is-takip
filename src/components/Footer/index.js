9e9f75ee637aed331711a2401299f211e1524815
Yavuzhan Aymak <yavuzhanaymak@gmail.com>
2022-10-16 07:59:15 +0300

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box';
 export default function Home() {
   return (
     <Box
+    style={{display:'flex', justifyContent:'center', alignItems:'center',}}
     sx={{
       width: '100%',
       height: 70,
@@ -11,11 +12,12 @@ export default function Home() {
       '&:hover': {
         backgroundColor: 'primary.main',
         opacity: [0.9, 0.8, 0.7],
+   
       },
     }}
   >
 
-    <h4>
+    <h4 style={{color:'purple'}}>
         Yavuzhan Aymak
     </h4>
     </Box>
